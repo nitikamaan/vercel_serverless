d7ddd35b07501aa28e5a153fe488adc87c70adc2
Nitika Maan <nitikamaan15209@gmail.com>
2026-06-21 22:48:56 +0530

    final fix

--- api/index.py
+++ api/index.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import json
 import numpy as np
 import os
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 BASE_DIR = os.path.dirname(__file__)
-DATA_PATH = os.path.join(os.path.dirname(__file__), "q-vercel-latency.json")
+DATA_PATH = os.path.join(BASE_DIR, "q-vercel-latency.json")
 
 with open(DATA_PATH, "r") as f:
     data = json.load(f)
-
 
 @app.post("/")
 async def analyze(request: Request):
