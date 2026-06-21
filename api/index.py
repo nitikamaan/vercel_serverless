b4d141690524f3d085a1cea45d3a682b99b25027
Nitika Maan <nitikamaan15209@gmail.com>
2026-06-21 22:34:30 +0530

    fix cors headers

--- api/index.py
+++ api/index.py
@@ -1,17 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
 import json
 import numpy as np
 import os
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
 
 BASE_DIR = os.path.dirname(__file__)
 DATA_PATH = os.path.join(BASE_DIR, "../q-vercel-latency.json")
